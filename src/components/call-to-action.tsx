f9b516ab331c0a3578df496fe0736aee9ad58ba2
Maksim Efremov <efremovmax2002@gmail.com>
2025-11-27 18:01:21 +0500

    feat: Мелкие доработки дизайна

--- src/components/call-to-action.tsx
+++ src/components/call-to-action.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const CallToAction = ({ title, description, buttonText, href }: Props) => {
   return (
-    <div className="text-card-foreground flex flex-wrap items-center justify-between gap-6 rounded-xl border border-dashed p-6">
+    <div className="text-card-foreground flex flex-wrap items-center justify-between gap-6 rounded-xl border-2 border-dotted p-6">
       <div className="space-y-2 flex-1 min-w-[60%]">
         <h3 className="text-lg font-semibold">{title}</h3>
         {description && <p className="text-muted-foreground">{description}</p>}
--- src/components/section.tsx
+++ src/components/section.tsx
@@ -15,8 +15,8 @@ const Section = ({ id, title, description, badge, children }: Props) => {
       className="mx-auto flex w-full max-w-6xl flex-col gap-8 px-6 py-20 sm:px-10 lg:px-16"
     >
       <div className="space-y-3 max-w-3xl">
-        <Badge variant="secondary">{badge}</Badge>
-        <h2 className="text-2xl font-semibold sm:text-3xl">{title}</h2>
+        <Badge variant="default">{badge}</Badge>
+        <h2 className="mt-2 text-2xl font-semibold sm:text-3xl">{title}</h2>
         <p className="text-lg text-muted-foreground ">{description}</p>
       </div>
       {children}
