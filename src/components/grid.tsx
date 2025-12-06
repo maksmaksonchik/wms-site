54449654dd7b36be469c046f05fab7bc14fbebb5
Maksim Efremov <efremovmax2002@gmail.com>
2025-12-06 18:03:41 +0500

    feat: Добавил прошлые школы, изменил пункты навигации

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -2,11 +2,12 @@ import { cn } from "@/lib/utils";
 
 type FixedGridProps = React.HTMLAttributes<HTMLDivElement> & {
   type: "fixed";
-  cols: 3 | 4 | 5;
+  cols: 2 | 3 | 4 | 5;
 };
 
 const FixedGrid = ({ children, cols, className, ...props }: FixedGridProps) => {
   const classes: Record<string, string> = {
+    2: "grid gap-6 grid-cols-1 md:grid-cols-2",
     3: "grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3",
     4: "grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4",
     5: "grid gap-2 sm:gap-4 grid-cols-3 sm:grid-cols-4 md:grid-cols-5",
