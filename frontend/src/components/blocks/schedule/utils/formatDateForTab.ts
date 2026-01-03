1a8d8fe35a5e0452ba62b0310ab1a8a6daa8da04
Maksim Efremov <75425602+maksmaksonchik@users.noreply.github.com>
2026-01-03 23:57:37 +0500

    fix: Сократил имя вкладок расписания, чтобы не было оверфлоу (#16)

--- frontend/src/components/blocks/schedule/utils/formatDateForTab.ts
+++ frontend/src/components/blocks/schedule/utils/formatDateForTab.ts
@@ -3,26 +3,12 @@
  */
 export const formatDateForTab = (dateString: string): string => {
   const days = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
-  const months = [
-    "Январь",
-    "Февраль",
-    "Март",
-    "Апрель",
-    "Май",
-    "Июнь",
-    "Июль",
-    "Август",
-    "Сентябрь",
-    "Октябрь",
-    "Ноябрь",
-    "Декабрь",
-  ];
 
   const date = new Date(dateString + "T00:00:00");
 
   const dayOfWeek = days[date.getDay()];
-  const day = date.getDate();
-  const month = months[date.getMonth()];
 
-  return `${dayOfWeek}, ${day} ${month}`;
+  const [_year, month, day] = dateString.split("-");
+
+  return `${dayOfWeek}, ${day}.${month}`;
 };
